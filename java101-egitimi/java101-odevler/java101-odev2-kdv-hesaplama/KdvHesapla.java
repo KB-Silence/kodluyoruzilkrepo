439101ca7d8acc9e6cbed157b2f4319799533c61
KB-Silence <berkcanserbest5@gmail.com>
2021-09-01 17:20:10 +0300

    java101 egitimi odev 2 duzenlendi

--- java101-egitimi/java101-odevler/java101-odev2-kdv-hesaplama/KdvHesapla.java
+++ java101-egitimi/java101-odevler/java101-odev2-kdv-hesaplama/KdvHesapla.java
@@ -3,13 +3,12 @@ import java.util.Scanner;
 public class KdvHesapla {
     public static void main(String[] args) {
 
-        // kullanıcıdan veri almak için scanner sınıfından 'inp' adında nesne oluşturuldu.
+        // kullanıcıdan veri almak için input sınıfından nesne oluşturuldu.
         Scanner inp = new Scanner(System.in);
 
         // kullanıcıdan veri almak ve kdv hesaplaması yapmak için değişkenler oluşturuldu.
+        double fiyat, sonuc, kdvTutari;
         int kdv;
-        double fiyat;
-        double sonuc;
 
         // kullanıcının giriş yapması için mesaj bastırıldı ve girilen değer değişkene atandı.
         System.out.print("Ürün Fiyatı Girin: ");
@@ -18,11 +17,13 @@ public class KdvHesapla {
 
         // fiyat tutarına göre kdv değeri belirlendi ve  hesaplaması yapıldı.
         kdv = (fiyat < 1000) ? 18 : 8;
-        sonuc = fiyat + (fiyat * kdv / 100);
+        kdvTutari = fiyat * kdv / 100;
+        sonuc = fiyat + kdvTutari;
 
         // hesaplama sonucu ve tutarlar ekrana bastırıldı.
         System.out.println("KDV'siz Fiyat: " + fiyat + " TL");
-        System.out.println("KDV Tutarı: %" + kdv);
+        System.out.println("KDV Oranı: %" + kdv);
+        System.out.println("KDV Tutarı: " + kdvTutari + " TL");
         System.out.println("KDV'li Fiyat: " + sonuc + " TL");
     }
 }
